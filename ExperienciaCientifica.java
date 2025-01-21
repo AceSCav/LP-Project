a061efdfc80b5930ccc670e394c6abbbe11d304b
Diogo Mota <diogomota@unknown8a90712e2f1e.lan>
2025-01-21 23:08:27 +0000

    Add/Remove Researchers

--- ExperienciaCientifica.java
+++ ExperienciaCientifica.java
@@ -43,5 +43,4 @@ public class ExperienciaCientifica
         equipments.add(equipment);
     }
     
-    
 }
--- Researcher.java
+++ Researcher.java
@@ -1,26 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Write a description of class Investigador here.
  *
  * @author (your name)
  * @version (a version number or a date)
  */
-    public class Researcher
-    {
-        public int id;
+      
+    public class Researcher{
+        
+        public static int nextId = 1;
+        public final int id;
         public String specialty;
         public String name;
+        public static final List<Researcher> researchers = new ArrayList<>();
         
         public Researcher(String specialty, String name){
+            this.id = nextId++;
             this.specialty = specialty;
-            this.nome = nome;
+            this.name = name;
         }
         
         public int getId(){
             return id;
-        }
-        
-        public void setId(int id){
-            this.id = id;
         }
            
         public String getSpecialty(){
@@ -38,5 +41,27 @@
         public void setName(String name){
             this.name = name;
         }
+        
+        public static void addResearcher(String specialty, String name){
+            Researcher newResearcher = new Researcher(specialty, name);
+            researchers.add(newResearcher);
+            System.out.println("Researcher added: " + newResearcher.getName());
+        }
+        
+        public static void removeResearcher(int id){
+            boolean found = false;
+            
+            for (int i = 0; i < researchers.size(); i++){
+                if (researchers.get(i).getId() == id){
+                    System.out.println("Researcher removed: " + researchers.get(i).getName());
+                    researchers.remove(i);
+                    found = true;
+                    break;
+                }
+            }
+            
+            if (!found){
+                System.out.println("No researcher found with ID:" + id);
+            }
+        }
     }
-}
